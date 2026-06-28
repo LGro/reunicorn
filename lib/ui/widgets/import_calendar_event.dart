ace26c23212d14f85f09590a6ce542dae83584d1
Lukas Grossberger <code@grossberger.xyz>
2026-06-28 10:59:48 +0200

    transition from cocoapods to swift package manager with veilid

--- lib/ui/widgets/import_calendar_event.dart
+++ lib/ui/widgets/import_calendar_event.dart
@@ -1,4 +1,4 @@
-// Copyright 2024 - 2025 The Reunicorn Authors. All rights reserved.
+// Copyright 2024 - 2026 The Reunicorn Authors. All rights reserved.
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 import 'dart:async';
